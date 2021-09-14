12ea78f6cadba6912c18cd492e577dcebb3cf2d3
Christian <arturogarcia_96@hotmail.com>
2021-09-14 15:46:54 -0500

    feat: Conexion a Eureka Server de MicroServicios product y shopping modificando archivos de configuracion

--- service-customer/src/main/java/academy/digitallab/store/servicecustomer/ServiceCustomerApplication.java
+++ service-customer/src/main/java/academy/digitallab/store/servicecustomer/ServiceCustomerApplication.java
@@ -2,7 +2,13 @@ package academy.digitallab.store.servicecustomer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/* Añadir la anotación @EnableEurekaCliente para la conexión del microservicio - proyecto a EurekaServer,
+ * después de añadir las lineas respectivas en el archivo de configuración (service-customer.yml en config-data).
+ * Las líneas a añadir estan en el archivo bootstrap.yml del presente microservicio - proyecto. */
+
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceCustomerApplication {
 
