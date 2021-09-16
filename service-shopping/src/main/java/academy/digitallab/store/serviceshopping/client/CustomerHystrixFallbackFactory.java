b0dedf961ab704fc6322d3863bce397fe6532d1b
Christian <arturogarcia_96@hotmail.com>
2021-09-15 20:48:42 -0500

    feat: customer fallaback in shopping-service

--- service-shopping/src/main/java/academy/digitallab/store/serviceshopping/client/CustomerHystrixFallbackFactory.java
+++ service-shopping/src/main/java/academy/digitallab/store/serviceshopping/client/CustomerHystrixFallbackFactory.java
@@ -8,7 +8,12 @@ import academy.digitallab.store.serviceshopping.model.Customer;
 @Component
 public class CustomerHystrixFallbackFactory implements CustomerClient{
 
-	/* Clase que envia un Customer con datos vacíos si la llamada de shopping a customer falla. */
+	/* No olvidar crear esta clase que implementa su respectivo Client.
+	 * Si el servicio de Customer se cayera y se ejecuta el de shopping, hystrix con el fallback nos permitira continuar
+	 * con el servicio, devolviendo un Customer con datos vacíos y el resto de resultados correctos. Con esto se evita
+	 * tumbarse todo el microservicio si una de sus dependencias falla. 
+	 * Dashboard: localhost:8093/hystrix
+	 * Ruta para revision: localhost:8093/actuator/hystrix.stream */
 	@Override
 	public ResponseEntity<Customer> getCustomer(Long idCustomer) {
 		
