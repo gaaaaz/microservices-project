85af045d9559012e5588d7b1cd594e258e7a00ff
Christian <arturogarcia_96@hotmail.com>
2021-09-16 19:24:05 -0500

    fix: some comments gateway

--- service-gateway/src/main/java/academy/digitallab/store/gateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/academy/digitallab/store/gateway/ServiceGatewayApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/* Gateway sirve para crear una puerta de entrada única a nuestros microservicios. Es decir, acceder a ellos a través
+ * de una ruta única. Para ello, en el proyecto creado, en la clase main colocaremos la anotación de Eureka, así como
+ * el archivo de config "service-gateway.yml" en config-data y el bootstrap.yml correspondiente en src/main/resources.
+ * En este caso después de implementar Gateway, podremos acceder a todos nuestros microservicios a través de
+ * localhost:8080. Cabe aclarar que antes de Gateway cada microservicio se accede a través de distintas rutas,
+ * como :8091, :8092, 8093... */
+
 @EnableEurekaClient
 @SpringBootApplication
 public class ServiceGatewayApplication {
