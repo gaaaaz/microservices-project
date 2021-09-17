c63f6ac12f2fbddef8a0edf440c5c92600e4b2bf
Christian <arturogarcia_96@hotmail.com>
2021-09-16 19:06:16 -0500

    fix: Remake service-gateway

--- service-gateway/src/main/java/academy/digitallab/store/gateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/academy/digitallab/store/gateway/ServiceGatewayApplication.java
@@ -2,13 +2,7 @@ package academy.digitallab.store.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-/* Lo único a realizar para habilitar Gateway como puerta única para nuestros microservicios es colocar en la clase main
- * la anotación @EnableEurekaClient y colocar la configuración correspondiente en service-gateway.yml en la carpeta
- * config-data, así como la configuración de boostrap.yml.
- * No olvidar pushear las configuraciones y reiniciar service-config y service-registry.*/
-@EnableEurekaClient
 @SpringBootApplication
 public class ServiceGatewayApplication {
 
